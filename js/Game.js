f59147d936ca09759411692a5859e036c9e8bda3
Dhreti <shikhawic@gmail.com>
2020-10-12 17:52:25 +0530

    animations

--- js/Game.js
+++ js/Game.js
@@ -30,25 +30,29 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1img);
     car2 = createSprite(300,200);
+    car2.addImage(car2img);
     car3 = createSprite(500,200);
+    car3.addImage(car3img);
     car4 = createSprite(700,200);
+    car4.addImage(car4img);
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-
+    
     Player.getPlayerInfo();
     
-    if(allPlayers !== undefined){
-      //var display_position = 100;
-      
+    if(allPlayers !== undefined){ //Game starts
+      background("brown");
+      image(trackimg,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -59,13 +63,15 @@ class Game {
         x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
+
         cars[index-1].x = x;
         cars[index-1].y = y;
 
+        //Identifying the currently active car
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
+          camera.position.y = cars[index-1].y;
         }
        
         //textSize(15);
@@ -79,6 +85,13 @@ class Game {
       player.update();
     }
 
+    if(player.distance>3500){
+      gameState=2;
+    }
     drawSprites();
+  }
+
+  end(){
+    console.log("Game Over");
   }
 }
